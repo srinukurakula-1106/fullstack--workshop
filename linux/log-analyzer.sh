73bdb72e46648f23ff81843661f0974ab8fe2648
srinukurakula-1106 <srinukurakula1106@gmail.com>
2025-12-24 11:04:04 +0530

    Initial commmit

---
+++ linux/log-analyzer.sh
@@ -0,0 +1,15 @@
+﻿echo "=============Log Analysis Report==========="
+file="C:\Users\srinu\Downloads\sample-log.txt"
+total=$(wc -l "C:\Users\srinu\Downloads\sample-log.txt")
+Info=$(grep -o "INFO" $file | wc -l)
+warn=$(grep -o "WARNING" $file | wc -l)
+error=$(grep -o "ERROR" $file | wc -l)
+echo "FILE:$file"
+echo "total Lines:$total"
+echo "Info:" $Info
+echo "warn:" $warn
+echo "err:" $error
+echo "-------------------------"
+echo UNIQUE IP ADRESS FOUND=$(grep -oE '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0->
+echo "======================================================="
+
